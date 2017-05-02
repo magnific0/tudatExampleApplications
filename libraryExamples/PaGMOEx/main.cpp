4fcf6f88c85c141a8da8d75ec48582d0e62b8d97
Jacco Geul <jacco@geul.net>
2017-05-02 16:37:28 +0200

    Also serialize the algorithm, to also restore the internal state of the rngs in the algo.

--- libraryExamples/PaGMOEx/main.cpp
+++ libraryExamples/PaGMOEx/main.cpp
@@ -28,6 +28,12 @@ using boost::format;
 //! Execute main
 int main( )
 {
+
+    // Get the source path of the application
+    std::string appPath( __FILE__ );
+    appPath = appPath.substr( 0, appPath.find_last_of( "/\\") + 1 );
+
+
     // Set the PRNG seed, such that results are reproducable
     int seed = 123456;
     pagmo::rng_generator::set_seed( seed );
@@ -58,6 +64,13 @@ int main( )
     // One generation per evolution step.
     algorithm::jde algo( 1 );
 
+    // Save the initial pop+prob+algo
+    std::ofstream ofs2( appPath + "ini.bak" );
+    boost::archive::text_oarchive oa2( ofs2 );
+    oa2 << pop << algo;
+    ofs2.close();
+
+
     unsigned int i = 0;
 
     // For 25 generation optimise the population
@@ -76,15 +89,14 @@ int main( )
     // instances classes) into a stream of bytes. This allows us to
     // save a population disk and reload it at another time (to
     // continue the optimization).
-    std::string appPath( __FILE__ );
-    appPath = appPath.substr( 0, appPath.find_last_of( "/\\") + 1 );
+
 
     // Save the population (contains also the problem) and some other
     // variable (just to show how its done).
     std::cout << "Saving the population" << std::endl;
     std::ofstream ofs( appPath + "pop.bak" );
     boost::archive::text_oarchive oa( ofs );
-    oa << i << bounds << pop;
+    oa << i << bounds << pop << algo;
     ofs.close();
 
     // Change the value of "i" and the population
@@ -96,11 +108,11 @@ int main( )
     std::cout << "Loading the population" << std::endl;
     std::ifstream ifs( appPath + "pop.bak" );
     boost::archive::text_iarchive ia( ifs );
-    ia >> i >> bounds >> pop;
+    ia >> i >> bounds >> pop >> algo;
     ifs.close( );
 
     // Resume the evolution
-    for( ; i < 70; i++ )
+    for( ; i < 50; i++ )
     {
         algo.evolve( pop );
         int c = pop.get_best_idx( );
@@ -112,6 +124,32 @@ int main( )
 
     std::cout << "How great that we were able to load the population and continue"
               << " where we left off!" << std::endl;
+
+    // Next initialize a new population and algorithm with the stored initial states.
+    std::ifstream ifs2( appPath + "ini.bak" );
+    boost::archive::text_iarchive ia2( ifs2 );
+
+    population pop2( prob, 8 );
+    algorithm::jde algo2( 1 );
+    ia2 >> pop2 >> algo2;
+    ifs2.close( );
+
+    // Evolve for the same number of generations
+    i = 0;
+    for( ; i < 50; i++ )
+    {
+        algo2.evolve( pop2 );
+    }
+
+    // Test if pop2 and pop are the same, based on decision vector and fitness value.
+    bool popeqp = true;
+    for(unsigned int j = 0; j < pop.size(); j++){
+        popeqp = popeqp && (pop.get_individual( j ).cur_x == pop2.get_individual( j ).cur_x
+                            && pop.get_individual( j ).cur_f == pop2.get_individual( j ).cur_f);
+    }
+    if(popeqp){
+        std::cout << "The populations are the same" << std::endl;
+    }
 
     return 0;
 }
